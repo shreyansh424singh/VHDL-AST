7168536e32b49413ff2b51c50c6b0538b373dc31
NischayDiwan <nischaydiwan1026@gmail.com>
2022-07-23 19:15:58 +0530

    make slight change

--- d.vhd
+++ d.vhd
@@ -8,41 +8,41 @@ end testbench;
 
 architecture fum of testbench is 
 
---component Program_Memory_64x32 is
---    port (  
---        rd        : out std_logic_vector(31 downto 0);
---        ad        : in std_logic_vector(7 downto 0)
---        ); 
---    end component; 
+component Program_Memory_64x32 is
+    port (  
+        rd        : out std_logic_vector(31 downto 0);
+        ad        : in std_logic_vector(7 downto 0)
+        ); 
+    end component; 
 
---    signal out1:  std_logic_vector (31 downto 0) := (others => '0');
---    signal in1:   std_logic_vector (7 downto 0) := (others => '0');
+    signal out1:  std_logic_vector (31 downto 0) := (others => '0');
+    signal in1:   std_logic_vector (7 downto 0) := (others => '0');
 
 begin 
 
---DUT: 
---    Program_Memory_64x32 
---    port map (out1, in1); 
+DUT: 
+    Program_Memory_64x32 
+    port map (out1, in1); 
 
---    process 
---    begin 
---    wait for 100 ns;
---    in1 <= "00000000";
---    wait for 100 ns;
+    process 
+    begin 
+    wait for 100 ns;
+    in1 <= "00000000";
+    wait for 100 ns;
 
---    in1 <= "00000100";
---    wait for 100 ns;
+    in1 <= "00000100";
+    wait for 100 ns;
 
---    in1 <= "00001000";
---    wait for 100 ns;
+    in1 <= "00001000";
+    wait for 100 ns;
 
---    in1 <= "00001100";
---    wait for 100 ns;
+    in1 <= "00001100";
+    wait for 100 ns;
 
---    in1 <= "00010000";
---    wait for 100 ns; 
+    in1 <= "00010000";
+    wait for 100 ns; 
 
---    ASSERT FALSE REPORT "Test done. Open EPWave to see signals." SEVERITY NOTE;
---    wait;
--- end process; 
+    ASSERT FALSE REPORT "Test done. Open EPWave to see signals." SEVERITY NOTE;
+    wait;
+ end process; 
 end;
